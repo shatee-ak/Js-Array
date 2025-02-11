dd45f874d6f12c4260f7f4d914475728eae54b70
shatee-ak <abithe99@gmail.com>
2025-02-11 12:57:12 +0600

    array

---
+++ script.js
@@ -0,0 +1,10 @@
+//1d Array(index)
+marks =[60,59,76,65];
+let sum = marks[2]+marks[3];
+console.log (sum);
+
+//Associative Array (objective Array)
+let man = {name:"Rahia", age:40, blood:"O+", phone:+880178252446, Address:"sylhet"}
+
+console.log(man.phone);
+console.log(man.age);
